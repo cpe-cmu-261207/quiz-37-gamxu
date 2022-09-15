47aa3e4b43f51eb87275df7d5b5860e2232aede8
gamxu <90751251+gamxu@users.noreply.github.com>
2022-09-15 14:34:19 +0700

    Update PostOwner.jsx

--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -4,7 +4,7 @@ export default function PostOwner(props) {
   return(
   <div className="vstack gap-3">
     <div className="d-flex align-items-center gap-3">
-      <Img
+      <img
         src="/profileImages/mypic.png"
         width="48"
         height="48"
@@ -15,7 +15,7 @@ export default function PostOwner(props) {
     </div>
     <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
     <div className="d-flex align-items-center gap-1">
-      <Img src="/like.svg" width={20}/>
+      <img src="/like.svg" width={20}/>
       <span className="text-muted">10K คน</span>
     </div>
     <hr className="m-0" />
